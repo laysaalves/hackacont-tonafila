bdb3d4226b4eac072ca1f5c8183949c5bee7d8ff
Kauan <cesarkauanferreira@hotmail.com>
2024-11-23 21:02:12 -0300

    feat: firebase config with env

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,9 +1,16 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebaseKeys.json");
+require('dotenv').config();
+
+const firebaseConfig = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://tonafila-64a15-default-rtdb.firebaseio.com",
+  credential: admin.credential.cert(firebaseConfig),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
 const db = admin.database();
