d5bdc9b1151240c2b2b44d777bab7122e8f564ac
Laysa <laysa.developer@gmail.com>
2024-11-23 21:40:39 -0300

    feat: add badge component

---
+++ frontend/components/Badge.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mt-4 flex items-center gap-2">
+    <div
+      class="w-[110px] h-[30px] bg-[#6B8ED3] rounded-full text-center mt-2 flex items-center justify-center font-light text-zinc-50"
+    >
+      {{ icon }}
+      {{ "" }}
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  text: string;
+  icon: string;
+}>();
+</script>
